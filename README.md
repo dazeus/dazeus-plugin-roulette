e046cdee34b0a268e525557c447397f1ddde62e7
Ruben Nijveld <ruben@gewooniets.nl>
2013-01-30 11:49:41 +0100

    Added command line options, some documentation, more config

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # DaZeus Roulette
-Randomly shoots you or maybe it doesn't if you're lucky! Use `}roulette`
+Randomly shoots you or maybe it doesn't if you're lucky! Provides the commands `}load-gun`
+to start a new game, `}pull` to pull the trigger and hope you don't get shot. Finally you
+can also use `}roulette` to try your luck once, outside of a game.
+
+
+## Installation
+
+    npm install dazeus-plugin-roulette
+
+## Running it
+If you don't need to change anything, just run this inside the directory it is installed in:
+
+    node index
+
+However some command line options are available, use this to see what is available:
+
+    node index --help
+
+
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 require('js-methods');
 var dazeus = require('dazeus');
 
+// 'constants', you may change these to more sensible defaults
 var NBULLETS = 6;
+var LOAD_GUN_CMD = 'load-gun';
+var PULL_CMD = 'pull';
+var ROULETTE_CMD = 'roulette';
 
+// lets parse command line args
+var optimist = dazeus.optimist();
+var argv = optimist
+    .usage("Usage: $0")
+    .describe("help", "Display this help message")
+    .argv;
+
+// show the help
+if (argv.help) {
+    optimist.showHelp();
+    process.exit();
+}
+var options = dazeus.options_from_argv(argv);
+
+// this array will store all active games
 var games = {};
 
-var client = dazeus.connect({path: '/tmp/dazeus.sock'}, function () {
-    client.onCommand('roulette', function (network, user, channel, command, args) {
+// create the client
+var client = dazeus.connect(options, function () {
+
+    /** Try your luck with a random try */
+    client.onCommand(ROULETTE_CMD, function (network, user, channel, command, args) {
         if (bulletAt() === 4) {
             kill(client, network, user, channel);
         } else {
@@ -14,7 +36,8 @@ var client = dazeus.connect({path: '/tmp/dazeus.sock'}, function () {
         }
     });
 
-    client.onCommand('load-gun', function (network, user, channel, command) {
+    /** Start a new game */
+    client.onCommand(LOAD_GUN_CMD, function (network, user, channel, command) {
         if (typeof games[network] === 'undefined') {
             games[network] = {};
         }
@@ -30,7 +53,8 @@ var client = dazeus.connect({path: '/tmp/dazeus.sock'}, function () {
         });
     });
 
-    client.onCommand('pull', function (network, user, channel, command) {
+    /** Pull the trigger */
+    client.onCommand(PULL_CMD, function (network, user, channel, command) {
         if (typeof games[network] === 'undefined') {
             games[network] = {};
         }
@@ -49,28 +73,68 @@ var client = dazeus.connect({path: '/tmp/dazeus.sock'}, function () {
                 } else {
                     click(client, network, user, channel);
                 }
+
+                // we'll just count down until we're at 0
                 games[network][where]--;
             }
         });
     });
 });
 
+/**
+ * Determine the location of the bullet.
+ * @return {Number} The location of the bullet (0...NBULLETS - 1)
+ */
 var bulletAt = function () {
     return Math.floor(Math.random() * NBULLETS);
 };
 
+/**
+ * Notify someone they've been killed.
+ * @param  {DaZeus} client  Client object
+ * @param  {String} network Network the user is on
+ * @param  {String} user    Name of the user
+ * @param  {String} channel Name of the channel the user is on
+ */
 var kill = function (client, network, user, channel) {
     client.reply(network, channel, user, "*boom*", false);
 };
 
+/**
+ * Notify someone they're lucky today, the gun only did 'click'.
+ * @param  {DaZeus} client  Client object
+ * @param  {String} network Network the user is on
+ * @param  {String} user    Name of the user
+ * @param  {String} channel Name of the channel the user is on
+ */
 var click = function (client, network, user, channel) {
     client.reply(network, channel, user, "_click_...", false);
 };
 
+/**
+ * Notify that a game hasn't been started, show them how to start a new one
+ * @param  {DaZeus} client  Client object
+ * @param  {String} network Network the user is on
+ * @param  {String} user    Name of the user
+ * @param  {String} channel Name of the channel the user is on
+ */
 var notstarted = function (client, network, user, channel) {
-    client.reply(network, channel, user, "A gun and some bullets are on the table, let's }load-gun...", false);
+    client.reply(
+        network,
+        channel,
+        user,
+        "A gun and some bullets are on the table, let's }" + LOAD_GUN_CMD + "...",
+        false
+    );
 };
 
+/**
+ * Notify that a new game is started
+ * @param  {DaZeus} client  Client object
+ * @param  {String} network Network the user is on
+ * @param  {String} user    Name of the user
+ * @param  {String} channel Name of the channel the user is on
+ */
 var started = function (client, network, user, channel) {
     client.reply(network, channel, user, "*rattle*... the gun is loaded!", false);
 };
